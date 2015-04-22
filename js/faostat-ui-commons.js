1ff5def9d72bff76f2de0af3e3384afe93798f7e
Kalimaha <guido.barbaglia@gmail.com>
2015-04-22 16:11:11 +0200

    Functions to invoke WDS services.

--- js/faostat-ui-commons.js
+++ js/faostat-ui-commons.js
@@ -1,10 +1,12 @@
-define(['jquery',
+define(['require',
+        'jquery',
         'handlebars',
         'text!faostat_ui_commons/html/templates.html',
+        'text!faostat_ui_commons/resources/schemas/wds.json',
         'i18n!faostat_ui_commons/nls/translate',
         'bootstrap',
         'sweetAlert',
-        'amplify'], function ($, Handlebars, templates, translate) {
+        'amplify'], function (Require, $, Handlebars, templates, wds_schema, translate) {
 
     'use strict';
 
@@ -14,8 +16,10 @@ define(['jquery',
 
             lang: 'en',
             lang_faostat: 'E',
+            prefix: 'faostat_ui_commons_',
             placeholder_id: 'faostat_ui_commons',
-            prefix: 'faostat_ui_commons_'
+            wds_schema: $.parseJSON(wds_schema),
+            wds_root: 'http://faostat3.fao.org/wds/rest'
 
         };
 
@@ -37,6 +41,75 @@ define(['jquery',
             case 'es': return 'S';
             default: return 'E';
         }
+    };
+
+    COMMONS.prototype.wdsclient = function(rest_service_name, parameters, callback) {
+
+        /* Root URL. */
+        var url = this.CONFIG.wds_root + '/' + rest_service_name + '/';
+
+        /* Load REST definition. */
+        var rest_parameters = this.CONFIG.wds_schema.properties[rest_service_name].properties;
+
+        try {
+
+            /* Check whether the CONFIG object contains all the required parameters for the REST. */
+            this.check_parameters(parameters, rest_parameters);
+
+            /* Create the URL by taking the parameters from the CONFIG object according to the JSON Schema definition. */
+            for (var i = 0 ; i < Object.keys(rest_parameters).length ; i++)
+                url += parameters[Object.keys(rest_parameters)[i]] + '/';
+
+            /* Define the HTTP method, GET by default, */
+            var method = this.CONFIG.wds_schema.properties[rest_service_name].method;
+            method = method != null ? method : 'GET';
+
+            /* Call WDS. */
+            $.ajax({
+
+                type: method,
+                url: url,
+
+                success: function (response) {
+
+                    /* Cast response to JSON, if needed. */
+                    var json = response;
+                    if (typeof json == 'string')
+                        json = $.parseJSON(response);
+
+                    /* Invoke user's callback. */
+                    if (callback != null)
+                        callback(json);
+
+                },
+
+                /* Default error handling. */
+                error: function(a) {
+                    swal({
+                        title: translate.error,
+                        type: 'error',
+                        text: a.responseText
+                    });
+                }
+
+            });
+
+        } catch (e) {
+
+            swal({
+                title: translate.error,
+                type: 'error',
+                text: e
+            });
+
+        }
+
+    };
+
+    COMMONS.prototype.check_parameters = function(parameters, rest_parameters) {
+        for (var i = 0 ; i < Object.keys(rest_parameters).length ; i++)
+            if (parameters[Object.keys(rest_parameters)[i]] == undefined)
+                throw translate.missing_parameter + Object.keys(rest_parameters)[i];
     };
 
     return new COMMONS();
--- nls/translate.js
+++ nls/translate.js
@@ -1,6 +1,7 @@
 define({
     root : {
-
+        error: 'FAOSTAT UI Commons Error',
+        missing_parameter: 'Missing parameter named: '
     },
     fr : true,
     es : true
